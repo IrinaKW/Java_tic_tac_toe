0be6ae4209843269a0e099d2a3607faf33537ef4
Irina White <75076636+IrinaKW@users.noreply.github.com>
2023-03-15 19:58:20 +0000

    add greetings method

--- TicTacToe.java
+++ TicTacToe.java
@@ -1,27 +1,36 @@
 import java.util.Scanner;
 
-public class TicTacToe{
-
-  public static void main(String[] args){
-    // Welcoming block
+public class TicTacToe {
+  
+  public static Scanner input = new Scanner(System.in);
+  
+  public static String nameGreetings(){
     System.out.println("Welcome to Tic Tac Toe!");
-    Scanner input = new Scanner(System.in);
     System.out.println("What is your name?");
-    String playerName = input.nextLine();
-    System.out.println(playerName+" you are player 1");
+    return input.nextLine();
+  }
 
-    //Selecting marker block
-    System.out.println("Choose your marker: O or X?");
+  public static void main(String[] args) {
+    // Welcoming block
+    String playerName = nameGreetings();
+    System.out.println(playerName + " you are player 1");
+    
+    // Selecting marker block
+    Scanner input = new Scanner(System.in);
+    System.out.println("Choose your marker: O or X?\n");
     String playerMarker = input.nextLine().toUpperCase();
-    
-    
-    if (playerMarker.equals("O") || playerMarker.equals("X")){
-      System.out.println(playerMarker+ " is a good choice");
-    } else {
-      System.out.println(playerMarker+ " is a wrong letter");
-    }
-    
 
-    
+    if (playerMarker.equals("O") || playerMarker.equals("X")) {
+      System.out.println(playerMarker + " is a good choice \n");
+    } else {
+      System.out.println(playerMarker + " is a wrong letter \n");
+    }
+
+    System.out.println("Choose your position \n"
+        + "1  2  3 \n"
+        + "4  5  6 \n"
+        + "7  8  9 \n");
+    String playerTurn = input.nextLine();
+
   }
 }
